6ac927c8a9f46f7f860d01e8ff9ec0f09cdd62c2
yoramclapper <yoram.t.c@gmail.com>
2025-03-07 10:59:18 +0100

    Added model to store django-app information

--- planeet_krankyoram/core/models.py
+++ planeet_krankyoram/core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class AppsLib(models.Model):
+    app_name = models.CharField(max_length=255)
+    display_name = models.CharField(max_length=255)
+    url = models.CharField(max_length=255)
+    show = models.BooleanField()
+
+    class Meta:
+        verbose_name = 'Apps lib'
+        verbose_name_plural = 'Apps lib'
+
+    def __str__(self):
+        return self.display_name
