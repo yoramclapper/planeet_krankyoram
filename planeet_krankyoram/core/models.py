f5152bb813d8d555afd773dd59472c7b3ad1e50e
yoramclapper <yoram.t.c@gmail.com>
2025-03-11 06:13:29 +0100

    Minor changes, renamed method and admin display name of 'AppsLib'

--- planeet_krankyoram/core/models.py
+++ planeet_krankyoram/core/models.py
@@ -4,7 +4,7 @@ from django.core.exceptions import ValidationError
 from django.core.validators import RegexValidator
 
 
-def validate_url(value):
+def validate_url_endpoints(value):
     if value[0] + value[-1] != '//':
         raise ValidationError("URL should start and end with '/'")
     else:
@@ -17,7 +17,7 @@ class AppsLib(models.Model):
     url = models.CharField(
         max_length=255,
         validators=[
-            validate_url,
+            validate_url_endpoints,
             RegexValidator(
                 regex=r'[^A-Za-z0-9_/]',
                 message=f"URL contains invalid characters not in '[A-Za-z0-9_/]'",
@@ -28,8 +28,8 @@ class AppsLib(models.Model):
     show = models.BooleanField()
 
     class Meta:
-        verbose_name = 'Apps lib'
-        verbose_name_plural = 'Apps lib'
+        verbose_name = 'Apps library'
+        verbose_name_plural = 'Apps library'
 
     def __str__(self):
         return self.display_name
