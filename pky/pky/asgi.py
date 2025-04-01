6e110f2c18b175d91be31fc14fb4a8676c12a9d1
yoramclapper <yoram.t.c@gmail.com>
2025-04-01 12:44:16 +0200

    Renamed project and app folder from 'planeet_krankyoram' to 'pky'

--- pky/pky/asgi.py
+++ pky/pky/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for planeet_krankyoram project.
+ASGI config for pky project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planeet_krankyoram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pky.settings')
 
 application = get_asgi_application()
--- pky/pky/wsgi.py
+++ pky/pky/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for planeet_krankyoram project.
+WSGI config for pky project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planeet_krankyoram.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pky.settings')
 
 application = get_wsgi_application()
